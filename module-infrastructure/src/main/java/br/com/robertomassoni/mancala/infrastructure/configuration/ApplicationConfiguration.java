a60862c5cb40d838f43cafda9eef49c7da65dec9
betomassoni <36550944+betomassoni@users.noreply.github.com>
2023-10-16 19:44:19 -0300

    feat: initial commit

---
+++ module-infrastructure/src/main/java/br/com/robertomassoni/mancala/infrastructure/configuration/ApplicationConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.robertomassoni.mancala.infrastructure.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+
+        return JsonMapper.builder()
+                .propertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy())
+                .build();
+    }
+}
