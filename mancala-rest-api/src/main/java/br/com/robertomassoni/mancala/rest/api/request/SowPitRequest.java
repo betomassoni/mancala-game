21500ea561d3aff02a32dd24e2689056193f9032
betomassoni <36550944+betomassoni@users.noreply.github.com>
2023-10-18 16:57:04 -0300

    feat: add validations on request

--- mancala-rest-api/src/main/java/br/com/robertomassoni/mancala/rest/api/request/SowPitRequest.java
+++ mancala-rest-api/src/main/java/br/com/robertomassoni/mancala/rest/api/request/SowPitRequest.java
@@ -1,6 +1,9 @@
 package br.com.robertomassoni.mancala.rest.api.request;
 
+import br.com.robertomassoni.mancala.core.annotation.ValueOfEnum;
+import br.com.robertomassoni.mancala.core.domain.enums.Player;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +13,8 @@ public class SowPitRequest {
     @NotEmpty
     private String gameId;
     @NotEmpty
+    @ValueOfEnum(enumClass = Player.class, message = "Must be a valid player (PLAYER_1 or PLAYER_2)")
     private String player;
-    @NotEmpty
+    @NotNull
     private Integer pitIndex;
 }
